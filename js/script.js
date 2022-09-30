74bb687a0db67c3da6fb063831eea91e90bdbf1e
lz-junior <correajr80@hotmail.com>
2022-09-29 21:43:18 -0300

    adição de nova cidade

--- js/script.js
+++ js/script.js
@@ -1,67 +1,108 @@
 const btn = document.getElementById("btnOn");
 const btnOff = document.getElementById("btnOff");
 const btnBroken = document.getElementById("btnBroken");
+const selectCity = document.getElementById("selectCity");
+
 const lampOn = document.getElementById("lampOn");
 const lampOff = document.getElementById("lampOff");
 const lampBroken = document.getElementById("lampBroken");
+
 const cityDay = document.getElementById("cityDay");
 const cityNight = document.getElementById("cityNight");
+const cityDayBrazil = document.getElementById("cityDayBrazil");
+const cityNightBrazil = document.getElementById("cityNightBrazil");
+
+var cd = cityDayBrazil;
+var cn = cityNightBrazil;
+var lac = lampOn;
+var lap = lampOff;
+var lq = lampBroken;
+
+
+// Change City
+selectCity.addEventListener("change", ()=>{
+
+    if (selectCity.value == 'newyork') {
+        selectCity.style.color = 'red';
+        changingBackground(cityDay, cityNight)
+    } else if (selectCity.value == 'brazil') {
+        selectCity.style.color = 'green';
+        changingBackground(cityDayBrazil, cityNightBrazil)
+    };
+});
+
+
 
 // Events
+// a = cidade dia
+// b = cidade noite
+// c = lamp acesa
+// d = lamp apagada
+// e = lamp quebrada
 var on = btn.addEventListener("click", (e)=> {
     e.preventDefault();
-    lightOn();
+    lightOn(cd, cn, lac, lap, lq);
 });
 
 var off = btnOff.addEventListener("click", (e)=> {
     e.preventDefault();
-    lightOff();
+    lightOff(cd, cn, lac, lap, lq);
 });
 
 lampOff.addEventListener("click", (e)=> {
     e.preventDefault();
-    changeLamp();
+    changeLamp(cd, cn, lac, lap, lq);
 });
 
 lampOn.addEventListener("click", (e)=> {
     e.preventDefault();
-    changeLamp();
+    changeLamp(cd, cn, lac, lap, lq);
 });
 
 btnBroken.addEventListener("click", (e)=> {
     e.preventDefault();
-    lightOff();
+    lightOff(cd, cn, lac, lap, lq);
 });
 
 
 // Functions
-function lightOn() {
-    lampOff.classList.add("hide");
-    lampOn.classList.remove("hide");
-    cityNight.classList.add("hide");
-    cityDay.classList.remove("hide");
-    lampBroken.classList.add("hide");
+// a = cidade dia
+// b = cidade noite
+// c = lamp acesa
+// d = lamp apagada
+// e = lamp quebrada
+function lightOn(a, b, c, d, e) {
+    d.classList.add("hide");
+    c.classList.remove("hide");
+    b.classList.add("hide");
+    a.classList.remove("hide");
+    e.classList.add("hide");
 };
 
-function lightOff() {
-    lampOff.classList.remove("hide");
-    lampOn.classList.add("hide");
-    cityNight.classList.remove("hide");
-    cityDay.classList.add("hide");
-    btn.innerText = "Acender";
-    lampBroken.classList.add("hide");
+function lightOff(a, b, c, d, e) {
+    d.classList.remove("hide");
+    c.classList.add("hide");
+    b.classList.remove("hide");
+    a.classList.add("hide");
+    e.classList.add("hide");
     btn.classList.remove("hide");
     btnOff.classList.remove("hide");
     btnBroken.classList.add("hide");
 };
 
-function changeLamp() {
-    lampOn.classList.add("hide");
-    lampOff.classList.add("hide");
-    lampBroken.classList.remove("hide");
-    cityDay.classList.add("hide");
-    cityNight.classList.remove("hide");
+function changeLamp(a, b, c, d, e) {
+    c.classList.add("hide");
+    d.classList.add("hide");
+    e.classList.remove("hide");
+    a.classList.add("hide");
+    b.classList.remove("hide");
     btn.classList.add("hide");
     btnOff.classList.add("hide");
     btnBroken.classList.remove("hide");
 };
+
+function changingBackground(cidadeDia, cidadeNoite) {
+    cd = cidadeDia;
+    cn = cidadeNoite;
+};
+
