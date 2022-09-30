50b9756c3b96877f8b3a71bba685a0d0d452cfd7
lz-junior <correajr80@hotmail.com>
2022-09-30 19:51:56 -0300

    criação da function da lampada

--- js/script.js
+++ js/script.js
@@ -12,37 +12,46 @@ const cityNight = document.getElementById("cityNight");
 const cityDayBrazil = document.getElementById("cityDayBrazil");
 const cityNightBrazil = document.getElementById("cityNightBrazil");
 
-var backgroundMain = window.document.body;
+var backgroundMain = document.body;
 
-var cd;
-var cn;
+var cd = {cityDay, cityDayBrazil}
+var cn = {cityNight, cityNightBrazil}
 var lac = lampOn;
 var lap = lampOff;
 var lq = lampBroken;
 
+funcionamentoLampada()
 
 // Change City
 selectCity.addEventListener("change", ()=>{
 
-    if (selectCity.value == 'newyork') {
+    switch (selectCity.value) {
 
-        selectCity.style.color = 'red';
-        changeCity(cityDay, cityNight)
-
-    } else if (selectCity.value == 'brazil') {
-
-        selectCity.style.color = 'green';
-        changeCity(cityDayBrazil, cityNightBrazil)
-
+        case 'newyork':
+            cityDayBrazil.classList.add("hide");
+            cityNightBrazil.classList.add("hide");
+            selectCity.style.color = 'red';
+            CN(cityDay, cityNight);
+            funcionamentoLampada(cityDay, cityNight)
+            lamOff(lac, lap, lq);
+            BtnLamp()
+            break
+        case 'brazil':
+            cityDay.classList.add("hide");
+            cityNight.classList.add("hide");
+            selectCity.style.color = 'green';
+            CN(cityDayBrazil, cityNightBrazil);
+            funcionamentoLampada(cityDayBrazil, cityNightBrazil)
+            lamOff(lac, lap, lq);
+            BtnLamp()
+            break
+        default:
+            break
     };
 });
 
-function changeCity(cidadeDia, cidadeNoite) {
-    backgroundMain.style.background = 'none'
-    cd = cidadeDia;
-    cn = cidadeNoite;
-    cn.classList.remove("hide");
-};
+// FUNCIONAMENTO DA LÂMPADA
+function funcionamentoLampada(a, b) {
 
 // Events
 // a = cidade dia
@@ -50,30 +59,39 @@ function changeCity(cidadeDia, cidadeNoite) {
 // c = lamp acesa
 // d = lamp apagada
 // e = lamp quebrada
-btnOn.addEventListener("click", (e)=> {
-    e.preventDefault();
-    lamOn(lac, lap, lq);
-});
-btnOff.addEventListener("click", (e)=> {
-    e.preventDefault();
-    lamOff(lac, lap, lq);
-});
-btnBroken.addEventListener("click", (e)=> {
-    e.preventDefault();
-    lamOff(lac, lap, lq);
-    BtnLamp();
-});
-lampOn.addEventListener("click", (e)=> {
-    e.preventDefault()
-    lamBroken(lac, lap, lq);
-    changeBtnBroken();
-});
-lampOff.addEventListener("click", (e)=> {
-    e.preventDefault()
-    lamBroken(lac, lap, lq);
-    changeBtnBroken();
-});
 
+    btnOn.addEventListener("click", (e)=> {
+        e.preventDefault();
+        lamOn(lac, lap, lq);
+        CD(a, b)
+    });
+
+    btnOff.addEventListener("click", (e)=> {
+        e.preventDefault();
+        lamOff(lac, lap, lq);
+        CN(a, b)
+    });
+    btnBroken.addEventListener("click", (e)=> {
+        e.preventDefault();
+        lamOff(lac, lap, lq);
+        BtnLamp();
+        CN(a, b)
+    });
+
+    lampOn.addEventListener("click", (e)=> {
+        e.preventDefault()
+        lamBroken(lac, lap, lq);
+        changeBtnBroken();
+        CN(a, b)
+    });
+
+    lampOff.addEventListener("click", (e)=> {
+        e.preventDefault()
+        lamBroken(lac, lap, lq);
+        changeBtnBroken();
+        CN(a, b)
+    });
+};
 
 // Functions
 // a = cidade dia
@@ -85,22 +103,26 @@ function lamOn(c, d, e) {
     c.classList.remove("hide");
     d.classList.add("hide");
     e.classList.add("hide");
+
 };
 function lamOff(c, d, e) {
     c.classList.add("hide");
     d.classList.remove("hide");
     e.classList.add("hide");
+
 };
 function lamBroken(c, d, e) {
     c.classList.add("hide");
     d.classList.add("hide");
     e.classList.remove("hide");
+
 };
 function CD(a, b) {
     a.classList.remove("hide");
     b.classList.add("hide");
 };
 function CN(a, b) {
+    backgroundMain.style.background = 'none'
     a.classList.add("hide");
     b.classList.remove("hide");
 };
@@ -114,43 +136,3 @@ function BtnLamp() {
     btnOff.classList.remove("hide");
     btnBroken.classList.add("hide");
 };
-
-
-
-
-
-
-
-
-// function lightOn(a, b, c, d, e) {
-//     d.classList.add("hide");
-//     c.classList.remove("hide");
-//     b.classList.add("hide");
-//     a.classList.remove("hide");
-//     e.classList.add("hide");
-// };
-
-// function lightOff(a, b, c, d, e) {
-//     d.classList.remove("hide");
-//     c.classList.add("hide");
-//     b.classList.remove("hide");
-//     a.classList.add("hide");
-//     e.classList.add("hide");
-//     btn.classList.remove("hide");
-//     btnOff.classList.remove("hide");
-//     btnBroken.classList.add("hide");
-// };
-
-// function changeLamp(a, b, c, d, e) {
-//     c.classList.add("hide");
-//     d.classList.add("hide");
-//     e.classList.remove("hide");
-//     a.classList.add("hide");
-//     b.classList.remove("hide");
-//     btn.classList.add("hide");
-//     btnOff.classList.add("hide");
-//     btnBroken.classList.remove("hide");
-// };
-
-
-
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 Itens para corrigir:
 
-• A mudança das cidades estão funcionando, porém só está funcionando uma vez.
+• Na página inicial ao quebrar ou apagar a lâmpada a imagem está sumindo.
 
-• A lâmpada não está apagando quando muda de cidade.
+• Iniciando a interação com a opção "New York", tudo ocorre bem, mas mudando para "Brazil", ao acender a lâmpada a cidade troca para "New York", mesmo estando selecionado "Brazil".
 
-• A imagem da cidade de dia ainda não está sendo mostrada quando a lâmpada acende.
+• Ao iniciar a interação com "Brazil", tudo está normal. Ao trocar para "New York" também fica normal, o problema volta quando tentamos novamente voltar ao "Brazil".
+
+• Ao iniciar ou selecionar a opção "New York", e depois ir para "Brazil", quebrando a lâmpada a imagem volta para "New York". Isso também acontece com o botão Off.
